bdaf0cb48ca6eebb498db19fc0bee21fabbda2ed
lu.bl <lublank@qq.com>
2016-12-12 20:35:33 +0800

    删除example，重新调整配置

--- ueditor.config.js
+++ ueditor.config.js
@@ -34,17 +34,97 @@
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的重新定义
         , toolbars: [[
-            'fullscreen', 'source', '|', 'undo', 'redo', '|',
-            'bold', 'italic', 'underline', 'fontborder', 'strikethrough', 'superscript', 'subscript', 'removeformat', 'formatmatch', 'autotypeset', 'blockquote', 'pasteplain', '|', 'forecolor', 'backcolor', 'insertorderedlist', 'insertunorderedlist', 'selectall', 'cleardoc', '|',
-            'rowspacingtop', 'rowspacingbottom', 'lineheight', '|',
-            'customstyle', 'paragraph', 'fontfamily', 'fontsize', '|',
-            'directionalityltr', 'directionalityrtl', 'indent', '|',
-            'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify', '|', 'touppercase', 'tolowercase', '|',
-            'link', 'unlink', 'anchor', '|', 'imagenone', 'imageleft', 'imageright', 'imagecenter', '|',
-            'simpleupload', 'insertimage', 'emotion', 'scrawl', 'insertvideo', 'music', 'attachment', 'map', 'gmap', 'insertframe', 'insertcode', 'webapp', 'pagebreak', 'template', 'background', '|',
-            'horizontal', 'date', 'time', 'spechars', 'snapscreen', 'wordimage', '|',
-            'inserttable', 'deletetable', 'insertparagraphbeforetable', 'insertrow', 'deleterow', 'insertcol', 'deletecol', 'mergecells', 'mergeright', 'mergedown', 'splittocells', 'splittorows', 'splittocols', 'charts', '|',
-            'print', 'preview', 'searchreplace', 'drafts', 'help'
+            'fullscreen',
+            'source', //源代码
+            '|',
+            'undo', //撤销
+            'redo', //重做
+            '|',
+            'bold', //加粗
+            'italic', //斜体
+            'underline', //下划线
+            'fontborder', //字符边框
+            'strikethrough', //删除线
+            'subscript', //下标
+            'superscript', //上标
+            'removeformat', //清除格式
+            'formatmatch', //格式刷
+            'autotypeset', //自动排版
+            'blockquote', //引用
+            'pasteplain', //纯文本粘贴模式
+            '|',
+            'forecolor', //字体颜色
+            'backcolor', //背景色
+            'insertorderedlist', //有序列表
+            'insertunorderedlist', //无序列表
+            'selectall', //全选
+            'cleardoc', //清空文档
+            '|',
+            'rowspacingtop', //段前距
+            'rowspacingbottom', //段后距
+            'lineheight', //行间距
+            '|',
+            'customstyle', //自定义标题
+            'paragraph', //段落格式
+            'fontfamily', //字体
+            'fontsize', //字号
+            '|',
+            'directionalityltr', //从左向右输入
+            'directionalityrtl', //从右向左输入
+            'indent', //首行缩进
+            '|',
+            'justifyleft', //居左对齐
+            'justifyright', //居右对齐
+            'justifycenter', //居中对齐
+            'justifyjustify', //两端对齐
+            '|',
+            'touppercase', //字母大写
+            'tolowercase', //字母小写
+            '|',
+            'link', //超链接
+            'unlink', //取消链接
+            'anchor', //锚点
+            '|',
+            'imagenone', //默认
+            'imageleft', //左浮动
+            'imageright', //右浮动
+            'imagecenter', //居中
+            '|',
+            'simpleupload', //单图上传
+            'emotion', //表情
+            'insertvideo', //视频
+            'map', //Baidu地图
+            'insertframe', //插入Iframe
+            'pagebreak', //分页
+            '|',
+            'horizontal', //分隔线
+            'date', //日期
+            'time', //时间
+            'spechars', //特殊字符
+            '|',
+            'inserttable', //插入表格
+            'deletetable', //删除表格
+            'insertparagraphbeforetable', //"表格前插入行"
+            'insertrow', //前插入行
+            'deleterow', //删除行
+            'insertcol', //前插入列
+            'deletecol', //删除列
+            'inserttitle', //插入标题
+            'deletecaption', //删除表格标题
+            'edittable', //表格属性
+            'edittd', //单元格属性
+            'mergecells', //合并多个单元格
+            'mergeright', //右合并单元格
+            'mergedown', //下合并单元格
+            'splittocells', //完全拆分单元格
+            'splittorows', //拆分成行
+            'splittocols', //拆分成列
+            'charts', // 图表
+            '|',
+            'print', //打印
+            'preview', //预览
+            'drafts', // 从草稿箱加载
+            'edittip ' //编辑提示
         ]]
         //当鼠标放在工具栏上时显示的tooltip提示,留空支持自动多语言配置，否则以配置值为准
         //,labelMap:{
